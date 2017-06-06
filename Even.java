de7301d357844e64b062976d0c248436665d11ae
yoges23 <yogivpy23@gmail.com>
2017-06-06 16:56:22 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Even
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+if(n%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
